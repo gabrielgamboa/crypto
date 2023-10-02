5f17ab9c2b6df9efc0c694cbeef352ea158c9dd0
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2023-10-02 17:14:03 -0300

    feat: create html table to list coin

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { BiSearch } from 'react-icons/bi'
 import styles from './home.module.css'
+import { Link } from 'react-router-dom'
 
 export function Home() {
     return (
@@ -10,6 +11,35 @@ export function Home() {
                     <BiSearch size={30} color="#FFF"/>
                 </button>
             </form>
+
+            <table>
+                <thead className={styles.thead}>
+                    <tr>
+                        <th scope='col'>Moeda</th>
+                        <th scope='col'>Valor Mercado</th>
+                        <th scope='col'>Preço</th>
+                        <th scope='col'>Volume</th>
+                    </tr>
+                </thead>
+                <tbody id='tbody'>
+                    <tr className={styles.trBody}>
+                        <td className={styles.tdLabel}>
+                            <Link to="/detail/btc">
+                                <span>Bitcoin</span> / BTC
+                            </Link>
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$19.000,00
+                        </td>
+                        <td className={styles.tdLabel}>
+                            R$29.000,00
+                        </td>
+                        <td className={styles.tdLabel}>
+                            <span>-5.29</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
     )
 }
